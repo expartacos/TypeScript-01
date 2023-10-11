665cedf6745d9cb0b85b4e538f7003b17c9aebd8
Rafael Pereira <rafaelpersan@gmail.com>
2023-10-11 09:34:17 -0300

    added TypeScript in project

---
+++ src/script.ts
@@ -0,0 +1,15 @@
+let num1 = document.getElementById('numero1') as HTMLInputElement;
+let num2 = document.getElementById('numero2') as HTMLInputElement;
+let botao = document.getElementById('calcular') as HTMLOutputElement;
+let res = document.getElementById('result') as HTMLOutputElement;
+
+
+function calcular (n1: number, n2: number){
+        return n1 + n2;
+}
+botao.addEventListener('click', function(){
+    res.innerHTML = calcular(parseInt(num1.value), parseInt(num2.value)).toString();
+
+})
+
+
