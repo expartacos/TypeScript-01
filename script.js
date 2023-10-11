bcdb4ce058aeb101ff5411cf5f1a6b3e0ceac323
Rafael Pereira <rafaelpersan@gmail.com>
2023-10-11 08:58:44 -0300

    remove parseInt

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ function calcular (n1, n2){
     if (typeof n1 == 'number' && typeof n2 == 'number'){ 
         return n1 + n2;
     }else {
-        n1 = parseInt(n1);
-        n2 = parseInt(n2);
-        return n1 + n2;
+        return +n1 + +n2;
     }
 }
 botao.addEventListener('click', function(){
